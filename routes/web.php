5fdd14f92f35981bc64420a13c688e6ecf0b409e
Viet Bao <baotranviet123ht@gmail.com>
2020-04-16 15:58:14 +0700

    Fix authentication

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('homepage');
 
 Route::group(['namespace' => 'Auth'],function(){
 	Route::get('/login', 'LoginController@index')->name('login');
 	Route::post('/login', 'LoginController@login');
+    Route::get('logout', 'LoginController@logout')->name('logout');
 	Route::get('/register', 'RegisterController@index')->name('register');
 	Route::post('/register', 'RegisterController@create');
 });
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'HomeController@index')->name('home');
